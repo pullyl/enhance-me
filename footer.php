06b47906f3410a4c256c48ebf053e0a2fb1eb153
pullyl <pullyl@gmail.com>
2017-04-24 12:29:44 -0400

    Initial commit of enhance-me
    A simple, blank Wordpress theme

---
+++ footer.php
@@ -0,0 +1,10 @@
+<div id="footer">
+	<p>
+		&copy; <?php echo date("Y"); ?> <?php bloginfo('name'); ?> | Powered by <a href="http://wordpress.org/">WordPress</a> | Theme by <a href="http://laurenepully.com">Lauren Pully</a> | <?php wp_loginout(); ?>
+	</p>
+</div>
+</div>
+
+<?php wp_footer(); ?>
+</body>
+</html>
